ec44253017d09976dd9678e250cad7cd2a7ca79d
Sergei Popov <p.sergey_n@vk.com>
2025-08-05 03:07:51 +0500

    Обновлен файл app.js: изменен тестовый номер карты и улучшена реализация алгоритма Луна для валидации номеров банковских карт. Добавлена функция luna, которая возвращает результат проверки номера карты.

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -1,23 +1,26 @@
-let cardNumber = "4561-2612-1234-5467";
+let cardNumber = "4561-2612-1234-5464";
 
-cardNumber = cardNumber.replaceAll("-", "");
 
-cardNumber = cardNumber.split('');
+const luna = (cardNumber) => {
+  cardNumber = cardNumber.replaceAll("-", "");
 
-for (let i = 0; i < cardNumber.length; i += 2) {
-  let firstStep = parseInt(cardNumber[i]) * 2;
-  
-  if (firstStep >= 9) {
-    firstStep = firstStep - 9;
+  cardNumber = cardNumber.split('');
+
+  for (let i = 0; i < cardNumber.length; i += 2) {
+    let firstStep = parseInt(cardNumber[i]) * 2;
+    
+    if (firstStep >= 9) {
+      firstStep = firstStep - 9;
+    }
+    
+    cardNumber[i] = firstStep.toString();
   }
-  
-  cardNumber[i] = firstStep.toString();
+
+  const sumNumbers = cardNumber.reduce((acc, value) => {
+    return acc = Number(acc) + Number(value);
+  })
+
+  return sumNumbers % 10 == 0
 }
 
-const sumNumbers = cardNumber.reduce((acc, value) => {
-  return acc = Number(acc) + Number(value);
-})
-
-const result = sumNumbers % 10 == 0
-
-console.log(result);
+console.log(luna(cardNumber));
